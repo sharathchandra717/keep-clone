1f11828e889a4cb3f4e7a39d6559538dbcb965c6
Pavan Bellamkonda <pbellamk@berkadia.com>
2019-03-25 15:46:15 +0530

    added cors

--- keep-clone-nodejs-app/src/index.js
+++ keep-clone-nodejs-app/src/index.js
@@ -4,6 +4,13 @@ const bodyparser = require('body-parser');
 const app = express();
 const port = 3000 || process.env.PORT;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
 app.use(bodyparser.json({ limit: '5mb' }));
 
 app.use((req, res, next) => {
